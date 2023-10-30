eb8b730f18aa86579ec38ce37d158820def10a75
Jpmarinw <124265259+Jpmarinw@users.noreply.github.com>
2023-10-30 17:03:24 -0400

    Commit 30-10-2023

--- src/main/java/edu/ifam/dra/chatfront/ChatFrontApplication.java
+++ src/main/java/edu/ifam/dra/chatfront/ChatFrontApplication.java
@@ -9,27 +9,13 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-import ifam.edu.dra.chatfront.model.Contato;
+import edu.ifam.dra.chatfront.model.Contato;
 
 @SpringBootApplication
 public class ChatFrontApplication {
-
 	private static final Logger log = LoggerFactory.getLogger(ChatFrontApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatFrontApplication.class, args);
-	}
-
-	@Bean
-	RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
-	}
-
-	@Bean
-	CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-		return args -> {
-			Contato contato = restTemplate.getForObject("http://localhost:8080/contato/1", Contato.class);
-			log.info(contato.toString());
-		};
 	}
 }
